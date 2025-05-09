d6b2e072488152976f80ca462a48af2c3ba45790
Jonathan Voss <k98kurz@gmail.com>
2025-05-09 13:15:21 -0400

    updated Coin model and added tests

--- easycoin/models/Coin.py
+++ easycoin/models/Coin.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from sqloquent import HashedModel, Default, RelatedCollection, RelatedModel
 from tapehash import tapehash3, work, calculate_difficulty
 from tapescript import Script
@@ -12,12 +13,13 @@ class Coin(HashedModel):
     connection_info: str = ''
     table: str = 'coins'
     id_column: str = 'id'
-    columns: tuple[str] = ('id', 'timestamp', 'lock', 'amount', 'nonce', 'wallet_id')
+    columns: tuple[str] = ('id', 'timestamp', 'lock', 'amount', 'details', 'nonce', 'wallet_id')
     columns_excluded_from_hash = ('wallet_id',)
     id: str
     timestamp: int
     lock: bytes
     amount: int
+    details: bytes|None|Default[None]
     nonce: int|Default[0]
     wallet_id: str|None
     origins: RelatedCollection
@@ -25,13 +27,31 @@ class Coin(HashedModel):
     wallet: RelatedModel
 
     @property
+    def id_bytes(self) -> bytes:
+        return bytes.fromhex(self.id)
+
+    @property
     def lock(self) -> Script:
         return Script.from_bytes(self.data['lock'])
     @lock.setter
     def lock(self, val: bytes|Script):
-        if isinstance(val, Script):
+        if isinstance(val, bytes):
+            self.data['lock'] = val
+        elif isinstance(val, Script):
             self.data['lock'] = val.bytes
-        self.data['lock']
+        else:
+            raise TypeError('lock must be bytes|Script')
+
+    @property
+    def details(self) -> dict|None:
+        if self.data['details'] is None:
+            return None
+        return packify.unpack(self.data['details'])
+    @details.setter
+    def details(self, val: dict):
+        if not type(val) is dict:
+            raise TypeError('details must be dict')
+        self.data['details'] = packify.pack(val)
 
     def pack(self) -> bytes:
         """Serialize to bytes."""
@@ -40,7 +60,7 @@ class Coin(HashedModel):
     @classmethod
     def unpack(cls, data: bytes, inject: dict = {}) -> 'Coin':
         """Deserialize from bytes."""
-        return cls(**packify.unpack(data, inject=inject))
+        return cls(packify.unpack(data, inject=inject))
 
     def mint_value(self) -> int:
         """Calculates the mint value of the coin (if it has one)."""
@@ -51,16 +71,35 @@ class Coin(HashedModel):
         return (val - _mint_difficulty) * 1000
 
     @classmethod
-    def mine(cls, lock: bytes, amount: int = 10000) -> 'Coin':
-        """Mines a coin with the `amount` of value."""
-        difficulty = (amount // 1000) + _mint_difficulty
+    def create(cls, lock: bytes|Script, amount: int) -> 'Coin':
+        """Creates a new coin that must be funded or mined."""
         ts = int(time())
-        coin = cls({
+        return cls({
             'timestamp': ts,
-            'lock': lock,
+            'lock': lock if type(lock) is bytes else lock.bytes,
             'amount': amount,
             'nonce': 0,
         })
+
+    @classmethod
+    def mine(cls, lock: bytes, amount: int = 10000) -> 'Coin':
+        """Mines a coin with the `amount` of value."""
+        difficulty = (amount // 1000) + _mint_difficulty
+        coin = cls.create(lock, amount)
         work(coin, lambda c: c.preimage(c.data), difficulty, tapehash3)
+        return coin
+
+    @classmethod
+    def stamp(
+        cls, lock: bytes, amount: int, n: str|bytes|int,
+        optional: dict[str, str|int|bool|bytes] = {}
+    ):
+        """Create a Stamp."""
+        coin = cls.create(lock, amount)
+        details = {'n': n, **optional}
+        if 'id' in details:
+            del details['id']
+        details['id'] = sha256(packify.pack(details)).digest()
+        coin.details = details
         return coin
 
---
+++ tests/test_coin.py
@@ -0,0 +1,104 @@
+from context import models
+from genericpath import isfile
+from hashlib import sha256
+from sqlite3 import OperationalError
+from tapehash import calculate_difficulty, tapehash3
+from tapescript import Script
+import os
+import sqloquent
+import unittest
+
+
+DB_FILEPATH = 'tests/test.db'
+MIGRATIONS_PATH = 'tests/migrations'
+ANYONE_CAN_SPEND_LOCK = Script.from_src('true')
+
+
+class TestCoin(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        models.Coin.connection_info = DB_FILEPATH
+        models.Txn.connection_info = DB_FILEPATH
+        models.Input.connection_info = DB_FILEPATH
+        models.Output.connection_info = DB_FILEPATH
+        models.Wallet.connection_info = DB_FILEPATH
+        sqloquent.DeletedModel.connection_info = DB_FILEPATH
+        super().setUpClass()
+
+    def setUp(self):
+        if isfile(DB_FILEPATH):
+            os.remove(DB_FILEPATH)
+        super().setUp()
+
+    def tearDown(self):
+        for file in os.listdir(MIGRATIONS_PATH):
+            if isfile(f'{MIGRATIONS_PATH}/{file}'):
+                os.remove(f'{MIGRATIONS_PATH}/{file}')
+        if isfile(DB_FILEPATH):
+            os.remove(DB_FILEPATH)
+        super().tearDown()
+
+    def automigrate(self):
+        sqloquent.tools.publish_migrations(MIGRATIONS_PATH)
+        tomigrate = [ 
+            models.Coin, models.Txn, models.Wallet,
+            models.Input, models.Output,
+        ]
+        for model in tomigrate:
+            name = model.__name__
+            m = sqloquent.tools.make_migration_from_model(model, name)
+            with open(f'{MIGRATIONS_PATH}/create_{name}.py', 'w') as f:
+                f.write(m)
+        sqloquent.tools.automigrate(MIGRATIONS_PATH, DB_FILEPATH)
+
+    def test_lock_property_serializes_properly(self):
+        c = models.Coin()
+        c.lock = Script.from_src('true')
+        assert type(c.lock) is Script
+        assert type(c.data['lock']) is bytes
+
+    def test_lock_property_raises_TypeError_for_bad_input(self):
+        c = models.Coin()
+        with self.assertRaises(TypeError):
+            c.lock = "not a script"
+
+    def test_details_property_serializes_properly(self):
+        c = models.Coin()
+        c.details = {
+            'id': sha256(b'test').digest()
+        }
+        assert type(c.details) is dict
+        assert type(c.data['details']) is bytes
+
+    def test_details_property_raises_TypeError_for_bad_inputs(self):
+        c = models.Coin()
+        with self.assertRaises(TypeError):
+            c.details = "Not a dict"
+
+    def test_create_result_serializes_and_deserializes_correctly(self):
+        c = models.Coin.create(ANYONE_CAN_SPEND_LOCK, 9999)
+        assert isinstance(c, models.Coin)
+        packed = c.pack()
+        unpacked = models.Coin.unpack(packed)
+        for k,v in c.data.items():
+            assert k in unpacked.data
+            assert unpacked.data[k] == v
+
+    def test_mine_result_passes_difficulty_threshold(self):
+        threshold = 128 + 10
+        c = models.Coin.mine(ANYONE_CAN_SPEND_LOCK)
+        assert calculate_difficulty(tapehash3(c.preimage(c.data))) >= threshold
+        assert c.mint_value() >= c.amount
+
+    def test_stamp_method_returns_coin(self):
+        c = models.Coin.stamp(ANYONE_CAN_SPEND_LOCK, 999, "test")
+        assert isinstance(c, models.Coin)
+        assert 'id' in c.details
+        assert type(c.details['id']) is bytes
+        assert 'n' in c.details
+        assert c.details['n'] == "test"
+
+
+if __name__ == '__main__':
+    unittest.main()
+
