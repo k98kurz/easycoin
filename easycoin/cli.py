2a5e32542cd61901f4e38353934ebd3c43e1fb80
Jonathan Voss <k98kurz@gmail.com>
2025-05-14 23:55:24 -0400

    updated pyproject and added CLI stub

---
+++ easycoin/cli.py
@@ -0,0 +1,86 @@
+from sys import argv
+
+
+def print_help():
+    print("Usage: easycoin --setup_wallet")
+    print("Usage: easycoin --query [coin|txn|utxo] [--id hex_id]")
+    print("Usage: easycoin --daemon")
+    print("\tRuns the node in headless mode.")
+    print("Usage: easycoin --interactive")
+    print("\tRuns the node with Textual console UI (requires optional cui dependency)")
+    print("Usage: easycoin --mine [--goal int_goal] [--wallet wallet_id]")
+    print("\tMines until the goal EC⁻¹ are ready to spend. Default behavior is")
+    print("\tto mine up to a balance of 100k EC⁻¹ in 10k coins for the first wallet")
+    print("\tthat has a readable pubkey. Will error if there is no wallet available.")
+    print("Usage: easycoin --help")
+    print("Usage: easycoin --version")
+    print("Aliases: -sw for --setup_wallet -q for --query; -d for --daemon;")
+    print("-i for --interactive; -m for --mine; -h for --help")
+
+
+def setup_wallet():
+    print("Not yet implemented")
+
+
+def query():
+    print("Not yet implemented")
+
+
+def daemon():
+    print("Not yet implemented")
+
+
+def interactive():
+    try:
+        from .cui import NodeApp
+    except ImportError:
+        print("Error: the optional cui dependency is missing.")
+        print("Install it with `pip install easycoin[cui]`.")
+        exit(4)
+
+    app = NodeApp()
+    app.run()
+
+
+def mine():
+    print("Not yet implemented")
+
+
+def print_version():
+    print("Not yet implemented")
+
+
+def run():
+    if '--setup_wallet' in argv or '-sw' in argv:
+        setup_wallet()
+        exit()
+
+    if '--query' in argv or '-q' in argv:
+        query()
+        exit()
+
+    if '--daemon' in argv or '-d' in argv:
+        daemon()
+        exit()
+
+    if '--mine' in argv or '-m' in argv:
+        mine()
+        exit()
+
+    if '--interactive' in argv or '-i' in argv:
+        interactive()
+        exit()
+
+    if '--help' in argv or '-h' in argv:
+        print_help()
+        exit()
+
+    if '--version' in argv:
+        version()
+        exit()
+
+    if len(argv) > 1:
+        print(f"Unrecognized argument: {argv[1]}")
+
+    print("Select a mode (use --help or -h for help text).")
+
