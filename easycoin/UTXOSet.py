c31f664a90b9b3c3724e9fa65ecb90fbcc62d2e9
Jonathan Voss <k98kurz@gmail.com>
2025-05-27 19:12:38 -0400

    further work on UTXOSet

--- easycoin/UTXOSet.py
+++ easycoin/UTXOSet.py
@@ -1,4 +1,4 @@
-from .models import Input, Output
+from .models import Coin, Input, Output
 from dataclasses import dataclass, field
 from typing import Protocol
 
@@ -73,8 +73,9 @@ class UTXOSet:
                 return False
         for oid in txn.output_ids:
             if oid in self.add_inputs:
-                print('line 76') if debut else ''
+                print('line 76') if debug else ''
 
+        # first database check: ensure non-ephemeral inputs have not been spent
         sub_i = {iid for iid in txn.input_ids if iid in self.sub_inputs}
         add_o = {iid for iid in txn.input_ids if iid in self.add_outputs}
         both = sub_i.union(add_o)
@@ -83,21 +84,52 @@ class UTXOSet:
             'id',
             list(set(txn.input_ids).difference(both))
         ).count():
-            print('line 86') if debug else ''
+            print('line 87') if debug else ''
             return False
 
+        # second database check: ensure inputs are unspent outputs
         if txn.input_ids:
             real_outputs = [o.id for o in Output.query().is_in('id', txn.input_ids).get()]
         else:
             real_outputs = []
         for iid in txn.input_ids:
             if iid not in real_outputs and iid not in self.add_outputs:
-                print(f'line 95; {iid=}') if debug else ''
+                print(f'line 97; {iid=}') if debug else ''
                 return False
 
         return True
 
-    def apply(self, txn: TxnProtocol):
+    def can_reverse(self, txn: TxnProtocol, debug: bool = False) -> bool:
+        """Determine if a txn can be reversed in the current UTXOSet.
+            This returns False if a `txn.output_ids[i]` is not an unspent
+            output or if a `txn.input_ids[i]` is not an input (spent);
+            otherwise returns True. First checks ephemeral UTXOSet
+            changes, then queries the database.
+        """
+        # check ephemeral data first
+        for oid in txn.output_ids:
+            if oid in self.add_inputs or oid in self.sub_outputs:
+                print('line 112') if debug else ''
+                return False
+        for iid in txn.input_ids:
+            if iid in self.add_outputs:
+                print('line 116') if debug else ''
+
+        sub_o = {oid for oid in txn.output_ids if oid in self.sub_outputs}
+        add_i = {oid for oid in txn.output_ids if oid in self.add_inputs}
+        both = sub_o.union(add_i)
+
+        # check database on non-ephemeral outputs: ensure they have not been spent
+        if len(set(txn.output_ids).difference(both)) and Input.query().is_in(
+            'id',
+            list(set(txn.output_ids).difference(both))
+        ).count():
+            print('line 126') if debug else ''
+            return False
+
+        return True
+
+    def apply(self, txn: TxnProtocol, coins: dict[str, Coin] = {}):
         """Attempt to apply the transaction, persisting the changes to
             the database. Raises `ValueError` if it cannot be applied or
             if there is ephemeral data in the `UTXOSet` instance.
@@ -112,25 +144,31 @@ class UTXOSet:
         # delete spent outputs and mark them as inputs; 
         if txn.input_ids:
             Output.query().is_in('id', txn.input_ids).delete()
-        Input.insert_many([
-            {'id': i} for i in txn.input_ids
-        ])
+            inputs = []
+            for i in txn.input_ids:
+                if i in coins:
+                    inputs.append(Input.from_coin(coins[i]).data)
+                else:
+                    inputs.append({'id': i})
+            Input.insert_many(inputs)
 
         # create new outputs
         if txn.output_ids:
             existing = [o.id for o in Output.query().is_in('id', txn.output_ids).get()]
-            Output.insert_many([
-                {'id': i} for i in txn.output_ids
-                if i not in existing
-            ])
+            outputs = []
+            for i in [i for i in txn.output_ids if i not in existing]:
+                if i in coins:
+                    outputs.append(Output.from_coin(coins[i]).data)
+                else:
+                    outputs.append({'id': i})
+            Output.insert_many(outputs)
 
-    def reverse(self, txn: TxnProtocol):
+    def reverse(self, txn: TxnProtocol, coins: dict[str, Coin] = {}):
         """Attempt to reverse (or un-apply) the transaction, persisting
             the changes to the database. Raises `ValueError` if it
             cannot be reversed or if there is ephemeral data in the
             `UTXOSet` instance.
         """
-        raise NotImplemented('this method implementation is incomplete')
         ephemeral = len(self.sub_outputs) + len(self.add_outputs)
         ephemeral = len(self.sub_inputs) + len(self.add_inputs)
         if ephemeral:
@@ -138,4 +176,19 @@ class UTXOSet:
         if not self.can_reverse(txn):
             raise ValueError('txn cannot be reversed')
 
+        # delete outputs
+        if txn.output_ids:
+            Output.query().is_in('id', txn.output_ids).delete()
+
+        # undelete inputs -- make them into outputs
+        if txn.input_ids:
+            Input.query().is_in('id', txn.input_ids).delete()
+            existing = [o.id for o in Output.query().is_in('id', txn.input_ids).get()]
+            outputs = []
+            for i in [i for i in txn.input_ids if i not in existing]:
+                if i in coins:
+                    outputs.append(Output.from_coin(coins[i]).data)
+                else:
+                    outputs.append({'id': i})
+            Output.insert_many(outputs)
 
--- tests/test_utxoset.py
+++ tests/test_utxoset.py
@@ -20,7 +20,7 @@ def automigrate():
     sqloquent.tools.publish_migrations(MIGRATIONS_PATH)
     tomigrate = [ 
         models.Coin, models.Txn, models.Wallet,
-        models.Input, models.Output,
+        models.Input, models.Output, models.TrustNet,
     ]
     for model in tomigrate:
         name = model.__name__
@@ -34,6 +34,7 @@ def setup_class():
     models.Txn.connection_info = DB_FILEPATH
     models.Input.connection_info = DB_FILEPATH
     models.Output.connection_info = DB_FILEPATH
+    models.TrustNet.connection_info = DB_FILEPATH
     models.Wallet.connection_info = DB_FILEPATH
     sqloquent.DeletedModel.connection_info = DB_FILEPATH
     if isfile(DB_FILEPATH):
@@ -45,10 +46,11 @@ def setup():
     models.Txn.query().delete()
     models.Input.query().delete()
     models.Output.query().delete()
+    models.TrustNet.query().delete()
     models.Wallet.query().delete()
 
 
-class TestInput(unittest.TestCase):
+class TestUTXOClasses(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         setup_class()
@@ -66,6 +68,30 @@ class TestInput(unittest.TestCase):
     def setUp(self):
         setup()
         super().setUp()
+
+    def test_Input_e2e(self):
+        trustnet = models.TrustNet.insert({
+            'name': 'Unit Test Net',
+            'lock': ANYONE_CAN_SPEND_LOCK.bytes,
+        })
+        net_state = b'\x00' * 32
+        c = models.Coin.create(ANYONE_CAN_SPEND_LOCK, 1000, trustnet.id, net_state)
+        c.save()
+        i = models.Input.insert({
+            'id': c.id,
+            'net_id': c.net_id,
+            'net_state': c.net_state,
+            'commitment': c.commitment(c.data),
+        })
+        assert i.check()
+        i.delete()
+        i = models.Input.from_coin(c).save()
+        assert i.check()
+        packed = i.pack()
+        assert len(packed) == 175, len(packed)
+        unpacked = models.Input.unpack(packed)
+        assert unpacked.id == i.id, (unpacked, i)
+        assert unpacked.check()
 
     def test_Input_relations_work(self):
         w = models.Wallet.create(SEED_PHRASE, PASSWORD)
@@ -78,25 +104,40 @@ class TestInput(unittest.TestCase):
         assert w2.id == w.id
         assert c2.id == c.id
 
+    def test_Output_e2e(self):
+        trustnet = models.TrustNet.insert({
+            'name': 'Unit Test Net',
+            'lock': ANYONE_CAN_SPEND_LOCK.bytes,
+        })
+        net_state = b'\x00' * 32
+        c = models.Coin.create(ANYONE_CAN_SPEND_LOCK, 1000, trustnet.id, net_state)
+        c.save()
+        o = models.Output.insert({
+            'id': c.id,
+            'net_id': c.net_id,
+            'net_state': c.net_state,
+            'commitment': c.commitment(c.data),
+        })
+        assert o.check()
+        o.delete()
+        o = models.Output.from_coin(c).save()
+        assert o.check()
+        packed = o.pack()
+        assert len(packed) == 175, len(packed)
+        unpacked = models.Output.unpack(packed)
+        assert unpacked.id == o.id, (unpacked, o)
+        assert unpacked.check()
 
-class TestUTXOSet(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        setup_class()
-        super().setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        for file in os.listdir(MIGRATIONS_PATH):
-            if isfile(f'{MIGRATIONS_PATH}/{file}'):
-                os.remove(f'{MIGRATIONS_PATH}/{file}')
-        if isfile(DB_FILEPATH):
-            os.remove(DB_FILEPATH)
-        super().tearDownClass()
-
-    def setUp(self):
-        setup()
-        super().setUp()
+    def test_Output_relations_work(self):
+        w = models.Wallet.create(SEED_PHRASE, PASSWORD)
+        w.save()
+        c = models.Coin.create(ANYONE_CAN_SPEND_LOCK, 1000)
+        c.save()
+        o = models.Output.insert({'id': c.id, 'wallet_id': w.id})
+        w2 = o.wallet
+        c2 = o.coin
+        assert w2.id == w.id
+        assert c2.id == c.id
 
     def test_can_apply_Txn_spending_existing_Output(self):
         u = easycoin.UTXOSet()
@@ -105,17 +146,108 @@ class TestUTXOSet(unittest.TestCase):
         t.input_ids = [o.id]
         assert u.can_apply(t)
 
+    def test_can_apply_Txn_spending_ephemeral_Output(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        t = models.Txn({'output_ids': oid})
+        u = u.after(t)
+        t = models.Txn({'input_ids': oid})
+        assert u.can_apply(t)
+
+    def test_not_can_apply_Txn_spending_nonexistant_Output(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        t = models.Txn({'input_ids': oid})
+        assert not u.can_apply(t)
+
+    def test_not_can_apply_Txn_spending_ephemerally_spent_Output(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        o = models.Output.insert({'id': oid})
+        t = models.Txn({'input_ids': oid})
+        assert u.can_apply(t)
+        u.add_inputs.add(oid)
+        assert not u.can_apply(t)
+        u.add_inputs.remove(oid)
+        u.sub_outputs.add(oid)
+        assert not u.can_apply(t)
+
+    def test_not_can_apply_Txn_funding_spent_Input(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        iid = '4321'
+        models.Input.insert({'id': oid})
+        t = models.Txn({'input_ids': iid, 'output_ids': oid})
+        assert not u.can_apply(t)
+
     def test_apply_Txn_adds_Inputs_and_Outputs_to_database(self):
         u = easycoin.UTXOSet()
         coin = models.Coin.mine(ANYONE_CAN_SPEND_LOCK)
         coin.save()
-        assert models.Input.query().count() == 0
-        assert models.Output.query().count() == 0
         t = models.Txn()
         t.output_ids = [coin.id]
+
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 0
         u.apply(t)
         assert models.Input.query().count() == 0
         assert models.Output.query().count() == 1
+        o = models.Output.query().first()
+        assert not o.check()
+        o.delete()
+
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 0
+        u.apply(t, {coin.id: coin})
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 1
+        o = models.Output.query().first()
+        assert o.check()
+
+    def test_can_reverse_Txn_with_existing_Inputs_and_Outputs(self):
+        u = easycoin.UTXOSet()
+        i = models.Input.insert({'id': '4321'})
+        o = models.Output.insert({'id': '1234'})
+        t = models.Txn({'input_ids': i.id, 'output_ids': o.id})
+        assert u.can_reverse(t)
+
+    def test_can_reverse_Txn_with_ephemeral_Inputs_and_Outputs(self):
+        u = easycoin.UTXOSet()
+        iid = '4321'
+        oid = '1234'
+        t = models.Txn({'input_ids': iid, 'output_ids': oid})
+        assert u.after(t).can_reverse(t)
+
+    def test_not_can_reverse_Txn_with_a_spent_Output(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        models.Input.insert({'id': oid})
+        t = models.Txn({'output_ids': oid})
+        assert not u.can_reverse(t)
+
+    def test_not_can_reverse_Txn_with_an_ephemerally_spent_Output(self):
+        u = easycoin.UTXOSet()
+        oid = '1234'
+        t1 = models.Txn({'output_ids': oid})
+        t2 = models.Txn({'input_ids': oid})
+        assert not u.after(t2).can_reverse(t1)
+
+    def test_reverse_Txn_undoes_apply_in_database(self):
+        u = easycoin.UTXOSet()
+        coin = models.Coin.mine(ANYONE_CAN_SPEND_LOCK)
+        coin.save()
+        t = models.Txn()
+        t.output_ids = [coin.id]
+
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 0
+        u.apply(t)
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 1
+
+        u.reverse(t, {coin.id: coin})
+        assert models.Input.query().count() == 0
+        assert models.Output.query().count() == 0
 
     def test_can_reapply_existing_Output(self):
         u = easycoin.UTXOSet()
