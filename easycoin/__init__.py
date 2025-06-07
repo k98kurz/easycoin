594ac53c73b4076a88cb489a456e7bf896c249ae
Jonathan Voss <k98kurz@gmail.com>
2025-06-07 15:38:30 -0400

    added version(); expose new db helpers

--- easycoin/__init__.py
+++ easycoin/__init__.py
@@ -1,5 +1,17 @@
 from .english import wordlist
 from .misc import microbench, calc_microbench_offset
-from .models import Coin, Txn, Input, Output, Wallet
+from .models import (
+    Coin, Txn, Input, Output, Wallet,
+    TrustNet, TrustNetFeature, Attestation, Confirmation, Snapshot,
+    set_connection_info, get_migrations, publish_migrations, automigrate,
+)
 from .UTXOSet import UTXOSet
+
+
+__version__ = '0.0.1'
+
+def version() -> str:
+    """Returns the version of the bookchain package."""
+    return __version__
+
 
